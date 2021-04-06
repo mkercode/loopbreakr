8de8cec414692acd8b5ebb634e9d1eb0a9d62999
mkercode <mkercode@gmail.com>
2021-04-05 22:47:13 -0400

    linked logo to scroll to top

--- loopbreakr-site/src/components/Navbar/index.js
+++ loopbreakr-site/src/components/Navbar/index.js
@@ -1,54 +1,37 @@
 import React from 'react'
 import {Nav, NavbarContainer, NavLogoContainer, NavLogoIcon, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements'
 import {FaBars} from 'react-icons/fa'
+import {animateScroll as scroll} from 'react-scroll'
 import Image from '../../images/loopbreakrLogo.svg'
 
 const Navbar = ({toggle}) => {
+    const toggleHome = () =>{
+        scroll.scrollToTop();
+    }
     return (
         <Nav> 
             <NavbarContainer>
                 <NavLogoContainer>
-                    <NavLogoIcon src={Image}/>
+                    <NavLogoIcon src={Image} to='/' onClick={toggleHome} />
                 </NavLogoContainer>
                 <MobileIcon onClick={toggle}>
                     <FaBars/>
                 </MobileIcon>
                 <NavMenu>
                     <NavItem>
-                        <NavLinks to='about' 
-                        smooth={true}
-                        duration={500}
-                        spy={true}
-                        exact="true"
-                        offset={-80}>About</NavLinks>
+                        <NavLinks to='about' smooth={true} duration={700} spy={true} exact="true" offset={-80}>About</NavLinks>
                     </NavItem>
 
                     <NavItem>
-                        <NavLinks to='projects'
-                        smooth={true}
-                        duration={500}
-                        spy={true}
-                        exact="true"
-                        offset={-80}>Projects</NavLinks>
+                        <NavLinks to='projects' smooth={true} duration={700} spy={true} exact="true" offset={-80}>Projects</NavLinks>
                     </NavItem>
 
                     <NavItem>
-                        <NavLinks to='contact'
-                        smooth={true}
-                        duration={500}
-                        spy={true}
-                        exact="true"
-                        offset={-78}
-                        >Contact</NavLinks>
+                        <NavLinks to='contact' smooth={true} duration={700} spy={true} exact="true" offset={-78}>Contact</NavLinks>
                     </NavItem>
 
                     <NavItem>
-                        <NavLinks to='donate'
-                        smooth={true}
-                        duration={500}
-                        spy={true}
-                        exact="true"
-                        offset={-80}>Donate</NavLinks>
+                        <NavLinks to='donate' smooth={true} duration={700} spy={true} exact="true" offset={-80}>Donate</NavLinks>
                     </NavItem>
                 </NavMenu>
             </NavbarContainer>
--- loopbreakr-site/src/components/Sidebar/index.js
+++ loopbreakr-site/src/components/Sidebar/index.js
@@ -10,39 +10,16 @@ const Sidebar = ({isOpen, toggle}) => {
             </Icon>
 
             <SidebarWrapper>
-
+                
                 <SidebarMenu>
-                    
-                    <SidebarLink to='about' onClick={toggle}
-                    spy={true}
-                    exact="true"
-                    offset={-80}>
-                        About
-                    </SidebarLink>
-
-                    <SidebarLink to='projects' onClick={toggle}
-                    spy={true}
-                    exact="true"
-                    offset={-80}>
-                        Projects
-                    </SidebarLink>
-
-                    <SidebarLink to='contact' onClick={toggle}
-                    spy={true}
-                    exact="true"
-                    offset={-78}>
-                        Contact
-                    </SidebarLink>
-
-                    <SidebarLink to='donate' onClick={toggle}
-                    spy={true}
-                    exact="true"
-                    offset={-80}>
-                        Donate
-                    </SidebarLink>
-
+                    <SidebarLink to='about' onClick={toggle} spy={true} exact="true" offset={-80}>About</SidebarLink>
+                    <SidebarLink to='projects' onClick={toggle} spy={true} exact="true" offset={-80}>Projects</SidebarLink>
+                    <SidebarLink to='contact' onClick={toggle} spy={true} exact="true" offset={-78}>Contact</SidebarLink>
+                    <SidebarLink to='donate' onClick={toggle} spy={true} exact="true" offset={-80}>Donate</SidebarLink>
                 </SidebarMenu>
+            
             </SidebarWrapper>
+
         </SidebarContainer>
     )
 }
