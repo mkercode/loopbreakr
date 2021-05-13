3349c48d70be69df78a6d690c6ed9ec22dbd9928
mkercode <mkercode@gmail.com>
2021-05-13 05:46:55 -0400

    Fixed scrolling issue when using react router

--- loopbreakr-site/src/App.js
+++ loopbreakr-site/src/App.js
@@ -4,17 +4,20 @@ import Home from './pages';
 import DonationForm from './pages/donationform.js';
 import cogstruct from './pages/cogstruct.js';
 import hocusfocus from './pages/hocusfocus.js';
+import ScrollToTop from './components/Common/ScrollToTop';
 
 
 function App() {
+  
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/donationform" component={DonationForm} exact/>
-        <Route path="/cogstruct" component={cogstruct} exact/>
-        <Route path="/hocusfocus" component={hocusfocus} exact/>
-      </Switch>
+      <ScrollToTop/>
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/donationform" component={DonationForm} exact/>
+          <Route path="/cogstruct" component={cogstruct} exact/>
+          <Route path="/hocusfocus" component={hocusfocus} exact/>
+        </Switch>
     </Router>
   );
 }
